87fc69af238572338b9272bfbb4b92e222911b41
emma <sonicjane2020@gmail.com>
2020-04-08 20:59:24 -0700

    added helmet, added basic file structure'

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const helmet = require('helmet')
+const userRouter = require('./users/userRouter');
 
 const server = express();
+
+server.use(helmet());
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +13,14 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+
+  const endpoint = req.originalUrl;
+
+  const method = req.method;
+
+
+  console.log(`${method}, ${endpoint}`);
+}
 
 module.exports = server;
